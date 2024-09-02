56deea989ec1428aa5a0f8166df332816a3d4991
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-02 16:46:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+let list=document.querySelector("#infi-list");
+function addListItem(i) {
+  const listItem = document.createElement('li');
+  listItem.textContent = `item ${i}`;
+  list.appendChild(listItem);
+}
+let i = 1
+for(; i <= 10; i++) {
+  addListItem(i);
+}
+
+list.addEventListener('scroll', function() {
+  
+	if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+   {
+    addListItem(i);
+	   i++;
+  }
+}
+});
+	
